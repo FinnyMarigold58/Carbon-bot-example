81099b3fc1982a932b89030845e363dcc352d4f6
Finny <59434139+FinnyMarigold58@users.noreply.github.com>
2024-09-15 20:18:40 -0500

    Fix mistake

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,15 @@ import { Client, ClientMode } from "@buape/carbon";
 import PingCommand from "./commands/ping.js";
 import { serve } from "bun";
 
-serve({
-
-    fetch(request, server) {
-        const client = new Client(
-{
-            mode: ClientMode.Bun, 
-            clientId: process.env.CLIENT_ID as string,
-            publicKey: process.env.PUBLIC_KEY as string, 
-            token: process.env.DISCORD_TOKEN as string,
-            port: 3000
-        },
-[new PingCommand()]);
-
-        const response = client.router.fetch(request, server);
-
-        return response;
+const client = new Client(
+    {
+        mode: ClientMode.Bun, 
+        clientId: process.env.CLIENT_ID as string,
+        publicKey: process.env.PUBLIC_KEY as string, 
+        token: process.env.DISCORD_TOKEN as string,
+        port: 3000
     },
-})
+[new PingCommand()]
+);
+
+serve(client.router)
